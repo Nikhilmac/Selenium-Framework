0d5485f4b7f7abf2e6f4268081e11788fb18e28e
Nikhilmac <nikhil.qa124@@gmail.com>
2020-04-25 17:16:38 +0530

    Chnages

--- selenium/src/test/java/com/automation/pages/BaseClass.java
+++ selenium/src/test/java/com/automation/pages/BaseClass.java
@@ -1,11 +1,13 @@
 package com.automation.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import com.automation.utility.BrowserFactory;
 
 public class BaseClass {
+	public WebDriver driver;
 
 	@BeforeClass
 	public void setup() {
--- selenium/src/test/java/com/automation/testcases/loginTest.java
+++ selenium/src/test/java/com/automation/testcases/loginTest.java
@@ -2,23 +2,16 @@ package com.automation.testcases;
 
 import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import com.automation.pages.BaseClass;
 import com.automation.pages.loginPage;
-import com.automation.utility.BrowserFactory;
-import com.automation.utility.ExcelDataProvider;
 
 public class loginTest extends BaseClass {
 
 	@Test
 	public void loginApp() throws InterruptedException, IOException {
-		//ExcelDataProvider excel = new ExcelDataProvider();
-
+		// ExcelDataProvider excel = new ExcelDataProvider();
 
 		loginPage loginpage = PageFactory.initElements(driver, loginPage.class);
 		/**
